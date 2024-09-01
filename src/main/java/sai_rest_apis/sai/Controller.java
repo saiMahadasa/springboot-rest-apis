6ccab08fd7d7a1d6bf228339b1d83d2ff3bd7c55
Sai Subrahmanyam Mahadasa <143134515+saiMahadasa@users.noreply.github.com>
2024-09-01 06:20:58 -0500

    post request integrated to hashmap

--- src/main/java/sai_rest_apis/sai/Controller.java
+++ src/main/java/sai_rest_apis/sai/Controller.java
@@ -5,20 +5,38 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("/users")
 public class Controller {
+	
+	
+	Map<String, RestUser> allUsers = new HashMap<>();
+
+	
     @GetMapping
-    public String getUsers() {
-        return "Hello world";
+    public Collection<RestUser> getUsers() {
+        return allUsers.values();
     }
     
     @PostMapping
-    public String postUsers() {
-    	return "Post method called";
+    public String postUsers(@RequestBody RestUser userDetails) {
+    	 // Add the new user
+        RestUser addValue = new RestUser();
+        addValue.setUserId(userDetails.getUserId());
+        addValue.setName(userDetails.getName());
+        addValue.setEmailId(userDetails.getEmailId());
+        allUsers.put(userDetails.getUserId(), addValue);
+
+        return "User added successfully!";
     }
     
     @PutMapping
---
+++ src/main/java/sai_rest_apis/sai/RestUser.java
@@ -0,0 +1,28 @@
+package sai_rest_apis.sai;
+
+public class RestUser {
+	
+	private String name;
+	private String UserId;
+	private String emailId;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUserId() {
+		return UserId;
+	}
+	public void setUserId(String userId) {
+		UserId = userId;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+}
